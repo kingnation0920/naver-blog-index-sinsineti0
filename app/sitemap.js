cb42a020f2cc5376c61acdf8212558a79026efe8
kingnation0920 <57610017+kingnation0920@users.noreply.github.com>
2026-06-17 08:37:25 +0900

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://naver-blog-index-sinsineti0.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'hourly',
+      priority: 1,
+    },
+  ];
+}
